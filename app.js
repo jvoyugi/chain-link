d9f1dedfe77181b05f3354aa1460d4c4eda396b9
Javan Oyugi <jooyugi@safaricom.co.ke>
2023-01-12 11:25:36 +0300

    add contact feature

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const express= require('express'); //createApplication
+const express = require('express'); //createApplication
 const app = express(); //createApplication
-var PORT = null;
-if(!process.env.PORT){
-    PORT=5500;
-}
-else{
-    PORT=process.env.PORT;
-}
+const indexRoute = require("./routes/index");
+app.set('view engine', 'ejs');
 
-app.listen(PORT,()=>{
+app.use('/', indexRoute)
+
+let PORT = null;
+if (!process.env.PORT) {
+    PORT = 5500;
+}
+else {
+    PORT = process.env.PORT;
+}
+app.listen(PORT, () => {
 
     console.log(`Server is starting on port ${PORT} `);
 })
